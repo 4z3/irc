4515a9f666eb04d4c3007c6aae95d6f9e27fed9b
tv <tv@nomic.retiolum>
2013-02-03 00:39:44 +0100

    if set, $use specifies which components to enable

--- index.js
+++ index.js
@@ -20,14 +20,37 @@ var log = require('./log')
 var tinc = require('./tinc')
 
 
-var config = new tinc.Config()
+var use = {
+  config: true,
+  daemon: true,
+  server: true,
+}
+if (typeof process.env.use === 'string') {
+  Object.keys(use).forEach(function (name) {
+    use[name] = false
+  })
+  process.env.use
+    .split(',')
+    .filter(function (x) { return !!x })
+    .forEach(function (name) {
+      if (use.hasOwnProperty(name)) {
+        use[name] = true
+      } else {
+        throw new Error('cannot use unknown module: ' + name)
+      }
+    })
+}
+log.info('use: ' + inspect(use))
+
+if (use.config) {
+  var config = new tinc.Config()
 
-config.watch(conf_dir)
+  config.watch(conf_dir)
 
-config.on('error', function (err) {
-  log.error('config: ' + err.message)
-  process.exit(1)
-})
+  config.on('error', function (err) {
+    log.error('config: ' + err.message)
+    process.exit(1)
+  })
 
   config.on('watching', function (uri) {
     log.info('watching ' + uri)
@@ -42,27 +65,29 @@ config.on('error', function (err) {
     log.unhandled([ 'host-unload', hostname].join(' '))
   })
 
-config.on('host-load', function (hostname, config) {
-  if (!state.hosts[hostname]) {
-    state.hosts[hostname] = { addresses: {} }
-  }
-  var host = state.hosts[hostname]
+  config.on('host-load', function (hostname, config) {
+    if (!state.hosts[hostname]) {
+      state.hosts[hostname] = { addresses: {} }
+    }
+    var host = state.hosts[hostname]
 
-  host.config = config
-})
+    host.config = config
+  })
+}
 
-var daemon = new tinc.Daemon()
+if (use.daemon) {
+  var daemon = new tinc.Daemon()
 
-daemon.start(daemon_options)
+  daemon.start(daemon_options)
 
-daemon.on('error', function (err) {
-  log.error('daemon: ' + err.message)
-  process.exit(1)
-})
+  daemon.on('error', function (err) {
+    log.error('daemon: ' + err.message)
+    process.exit(1)
+  })
 
-process.on('exit', function (code, signal) {
-  daemon.stop()
-})
+  process.on('exit', function (code, signal) {
+    daemon.stop()
+  })
 
   daemon.on('ready', function () {
     log.info('daemon ready')
@@ -82,19 +107,21 @@ process.on('exit', function (code, signal) {
   daemon.on('stopped', function () {
     log.info('daemon stopped')
   })
+}
 
-var server = new tinc.Server()
+if (use.server) {
+  var server = new tinc.Server()
 
-server.listen(informer_socket)
+  server.listen(informer_socket)
 
-server.on('error', function (err) {
-  log.error('server: ' + err.message)
-  process.exit(1)
-})
+  server.on('error', function (err) {
+    log.error('server: ' + err.message)
+    process.exit(1)
+  })
 
-process.on('exit', function (code, signal) {
-  server.stop()
-})
+  process.on('exit', function (code, signal) {
+    server.stop()
+  })
 
   server.on('listening', function (uri) {
     log.info('listening ' + uri)
@@ -115,24 +142,25 @@ process.on('exit', function (code, signal) {
     log.info('server stopped')
   })
 
-server.on('host-up', function (hostname, remoteAddress, remotePort) {
-  if (!state.hosts[hostname]) {
-    state.hosts[hostname] = { addresses: {} }
-  }
-  var host = state.hosts[hostname]
+  server.on('host-up', function (hostname, remoteAddress, remotePort) {
+    if (!state.hosts[hostname]) {
+      state.hosts[hostname] = { addresses: {} }
+    }
+    var host = state.hosts[hostname]
 
-  host.addresses[remoteAddress] = remotePort
-  host.status = 'online'
-})
-server.on('host-down', function (hostname, remoteAddress, remotePort) {
-  if (!state.hosts[hostname]) {
-    state.hosts[hostname] = { addresses: {} }
-  }
-  var host = state.hosts[hostname]
+    host.addresses[remoteAddress] = remotePort
+    host.status = 'online'
+  })
+  server.on('host-down', function (hostname, remoteAddress, remotePort) {
+    if (!state.hosts[hostname]) {
+      state.hosts[hostname] = { addresses: {} }
+    }
+    var host = state.hosts[hostname]
 
-  host.addresses[remoteAddress] = remotePort
-  host.status = 'offline'
-})
+    host.addresses[remoteAddress] = remotePort
+    host.status = 'offline'
+  })
+}
 
 
 process.on('exit', function (code, signal) {
